5f82b28787e1e7874a41c3a3de71c8cdd7a6c3f0
Smit Harshadkumar Vyas <smitvyas83@gmail.com>
2024-05-26 20:07:34 -0600

    First Commit

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "./studentInfo";
+
+
+export default function Page() {
+    return(
+        <main className="h-screen">
+            <h1 className="text-x1">Shopping List </h1>
+            <StudentInfo />
+        </main>
+    );
+}
---
+++ app/week-2/studentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <p>Smit Vyas</p>
+            <Link href= "https://github.com/Smitvyas01/cprg306-assignments">GitHub Link </Link>
+        </main>
+    );
+}
